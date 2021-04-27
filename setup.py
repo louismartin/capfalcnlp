1c996839abe67063c85fd620110fa74b61d6d6ef
louismartin <louismartincs@gmail.com>
2021-04-27 02:38:53 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name="capfalcnlp",
+    version="0.1",
+    description="NLP tools for the Cap'FALC project.",
+    author="Louis Martin",
+    author_email="louismartincs@gmail.com",
+    packages=["capfalcnlp"],
+)
