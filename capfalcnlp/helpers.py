5757ff73c915947587a6dc77e6e1a8e64345ecbf
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 00:00:18 +0000

    Adding numeric_owner as keyword arguement

--- capfalcnlp/helpers.py
+++ capfalcnlp/helpers.py
@@ -197,7 +197,7 @@ def untar(compressed_path, output_dir):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(f, output_dir)
